3779b025817d6cf44428aefda7c2e53d4b2870fd
Zhishi <whfuyn@outlook.com>
2022-07-30 23:07:57 +0800

    Add PID allocator

---
+++ os/src/task/pid.rs
@@ -0,0 +1,41 @@
+use alloc::vec::Vec;
+use spin::Mutex;
+
+static PID_ALLOCATOR: Mutex<PidAllocator> = Mutex::new(PidAllocator::new());
+
+fn pid_alloc() -> Pid {
+    PID_ALLOCATOR.lock().alloc()
+}
+
+pub struct Pid(pub usize);
+
+impl Drop for Pid {
+    fn drop(&mut self) {
+        PID_ALLOCATOR.lock().free(self.0)
+    }
+}
+
+pub struct PidAllocator {
+    current: usize,
+    recycled: Vec<usize>,
+}
+
+impl PidAllocator {
+    const fn new() -> Self {
+        Self { current: 1, recycled: Vec::new() }
+    }
+
+    fn alloc(&mut self) -> Pid {
+        if let Some(pid) = self.recycled.pop() {
+            Pid(pid)
+        } else {
+            let pid = self.current;
+            self.current += 1;
+            Pid(pid)
+        }
+    }
+
+    fn free(&mut self, pid: usize) {
+        self.recycled.push(pid);
+    }
+}
